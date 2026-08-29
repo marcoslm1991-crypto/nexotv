8a2124bf05f50bd9374824f2b28f5a74efb75088
marcoslm1991-crypto <marcoslm1991@gmail.com>
2026-08-29 20:01:34 -0300

    Update backend/src/common/types/index.ts

---
+++ backend/src/common/types/index.ts
@@ -0,0 +1,7 @@
+export * from './auth.types';
+export * from './plans.types';
+export * from './subscriptions.types';
+export * from './profiles.types';
+export * from './streams.types';
+export * from './tv.types';
+export * from './vod.types';

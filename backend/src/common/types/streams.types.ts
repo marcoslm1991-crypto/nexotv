9551d714ae73f8807d7301a14b50088f60f3473c
marcoslm1991-crypto <marcoslm1991@gmail.com>
2026-08-29 20:01:37 -0300

    Update backend/src/common/types/streams.types.ts

---
+++ backend/src/common/types/streams.types.ts
@@ -0,0 +1,14 @@
+export interface StreamAuthorizeRequest {
+  profile_id: string;
+  device_name: string;
+  device_uuid: string;
+  content_id?: string;
+}
+
+export interface StreamAuthorizeResponse {
+  authorized: boolean;
+  active_stream_id?: string;
+  message?: string;
+  max_screens?: number;
+  current_active_screens?: number;
+}

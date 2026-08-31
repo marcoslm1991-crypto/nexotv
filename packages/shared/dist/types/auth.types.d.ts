ff3122ea7975e8f0b0f1fa3d0ee705e914dc0ea1
marcoslm1991-crypto <marcoslm1991@gmail.com>
2026-08-31 15:30:54 -0300

    Update packages/shared/dist/types/auth.types.d.ts

---
+++ packages/shared/dist/types/auth.types.d.ts
@@ -0,0 +1,19 @@
+export declare enum UserRole {
+    CLIENT = "CLIENT",
+    ADMIN = "ADMIN"
+}
+export interface UserPayload {
+    id: string;
+    alias: string;
+    name?: string;
+    role: UserRole;
+    is_active: boolean;
+}
+export interface LoginResponse {
+    access_token: string;
+    user: UserPayload;
+    subscription_status?: string;
+    days_until_expiration?: number;
+    warning_message?: string;
+}
+//# sourceMappingURL=auth.types.d.ts.map

b850231a1f59d3b0987d63206c8d1a97d716e16e
marcoslm1991-crypto <marcoslm1991@gmail.com>
2026-08-31 15:27:03 -0300

    Update backend/dist/packages/shared/src/types/subscriptions.types.js

---
+++ backend/dist/packages/shared/src/types/subscriptions.types.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SubscriptionStatus = void 0;
+var SubscriptionStatus;
+(function (SubscriptionStatus) {
+    SubscriptionStatus["VIGENTE"] = "VIGENTE";
+    SubscriptionStatus["PROXIMO_A_VENCER"] = "PROXIMO_A_VENCER";
+    SubscriptionStatus["VENCIDO"] = "VENCIDO";
+    SubscriptionStatus["SUSPENDIDO"] = "SUSPENDIDO";
+})(SubscriptionStatus || (exports.SubscriptionStatus = SubscriptionStatus = {}));
+//# sourceMappingURL=subscriptions.types.js.map

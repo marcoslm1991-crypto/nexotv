753aebe640596c577aff4a2c24661fb0629038c4
marcoslm1991-crypto <marcoslm1991@gmail.com>
2026-08-31 15:25:52 -0300

    Update backend/dist/common/types/auth.types.js

---
+++ backend/dist/common/types/auth.types.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserRole = void 0;
+var UserRole;
+(function (UserRole) {
+    UserRole["CLIENT"] = "CLIENT";
+    UserRole["ADMIN"] = "ADMIN";
+})(UserRole || (exports.UserRole = UserRole = {}));
+//# sourceMappingURL=auth.types.js.map

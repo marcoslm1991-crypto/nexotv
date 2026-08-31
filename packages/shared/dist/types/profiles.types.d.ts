26cda52a83beb768fe1524e0ade79c67f357f116
marcoslm1991-crypto <marcoslm1991@gmail.com>
2026-08-31 15:31:04 -0300

    Update packages/shared/dist/types/profiles.types.d.ts

---
+++ packages/shared/dist/types/profiles.types.d.ts
@@ -0,0 +1,8 @@
+export interface ProfileDto {
+    id: string;
+    user_id: string;
+    name: string;
+    avatar_url?: string;
+    created_at: string;
+}
+//# sourceMappingURL=profiles.types.d.ts.map

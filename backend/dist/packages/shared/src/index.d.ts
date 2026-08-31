29d8bc1133cea3bfb41648195d95d554c624e92e
marcoslm1991-crypto <marcoslm1991@gmail.com>
2026-08-31 15:26:42 -0300

    Update backend/dist/packages/shared/src/index.d.ts

---
+++ backend/dist/packages/shared/src/index.d.ts
@@ -0,0 +1,7 @@
+export * from './types/auth.types';
+export * from './types/plans.types';
+export * from './types/subscriptions.types';
+export * from './types/profiles.types';
+export * from './types/streams.types';
+export * from './types/tv.types';
+export * from './types/vod.types';

d5cf81e9026b60ee7a0769a32f971a7a2283b745
marcoslm1991-crypto <marcoslm1991@gmail.com>
2026-08-31 15:30:59 -0300

    Update packages/shared/dist/types/plans.types.d.ts

---
+++ packages/shared/dist/types/plans.types.d.ts
@@ -0,0 +1,10 @@
+export interface PlanDto {
+    id: string;
+    name: string;
+    code: string;
+    max_screens: number;
+    max_profiles: number;
+    description?: string;
+    is_active: boolean;
+}
+//# sourceMappingURL=plans.types.d.ts.map

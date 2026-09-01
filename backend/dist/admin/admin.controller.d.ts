d824364de5d3f164a399ea966bd71a0cb28af62b
marcoslm1991-crypto <marcoslm1991@gmail.com>
2026-09-01 12:06:19 -0300

    Update backend/dist/admin/admin.controller.d.ts

--- backend/dist/admin/admin.controller.d.ts
+++ backend/dist/admin/admin.controller.d.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from 'express';
 export declare class AdminController {
-    getAdminRoot(res: Response): void | Response<any, Record<string, any>>;
+    getAdminRootNoSlash(req: Request, res: Response): void | Response<any, Record<string, any>>;
     getAdminAsset(req: Request, res: Response): void | Response<any, Record<string, any>>;
     private serveFile;
 }
